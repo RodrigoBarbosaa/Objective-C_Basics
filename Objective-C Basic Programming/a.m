40729eeb95e01dfc50a91d097c13632bc3cb3048
RodrigoBarbosaa <rodrigobarbosadeoliveira29@gmail.com>
2024-10-09 23:25:52 -0300

    Arrays & Set

---
+++ Objective-C Basic Programming/a.m
@@ -0,0 +1,8 @@
+//
+//  a.m
+//  Objective-C Basic Programming
+//
+//  Created by Rodrigo Barbosa on 09/10/24.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Objective-C Basic Programming/main.m
@@ -0,0 +1,39 @@
+//
+//  main.m
+//  Objective-C Basic Programming
+//
+//  Created by Rodrigo Barbosa on 09/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // ARRAYS & SET
+        NSArray* myArr;
+        myArr = [[NSArray alloc] initWithObjects:@"Olá mundo", @"texto", nil];
+        
+        NSLog(@"Object at index 1 is: %@", [myArr objectAtIndex:1]);
+        NSLog(@"There are %lu objects in my array", myArr.count);
+        
+        
+        NSMutableArray* myMutableArray;
+        myMutableArray = [[NSMutableArray alloc] initWithObjects: @"abcde", @"fghi", nil];
+        [myMutableArray addObject:@"jklm"];
+        
+        NSSet* mySet = [[NSSet alloc] initWithObjects:@"Sunday", @"Monday", @"Friday", nil];
+        
+        for (NSString* any in mySet) {
+            if ([any isEqualToString:@"Friday"]) {
+                NSLog(@"Its friday!");
+            } else {
+                NSLog(@"Isnt friday ;-;");
+            }
+        }
+        
+        
+        
+    }
+    return 0;
+}
